9abecafe2a54d090aba050077fa7c3795a79a426
Michael V <michaelvrld@gmail.com>
2015-11-23 01:35:16 +0100

    Create highcharts_playerbase.php

---
+++ highcharts_playerbase.php
@@ -0,0 +1,63 @@
+<?php
+    include 'inc/SACNRMonitor.php';
+
+    define("SERVER_ID", 1742168);
+    define("PAST_HOURS", 12);
+
+    $monitor = new SACNRMonitor;
+    $json_query = json_encode((array)$monitor->get_query_by_id(SERVER_ID));
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Playerbase</title>
+    </head>
+    <body>
+        <section id="graph"></section>
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="http://code.highcharts.com/highcharts.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var data = <?php echo $json_query ?>, hours = [], players = [], date;
+                
+                for (var i = data.length - (<?php echo PAST_HOURS; ?>*4), j = data.length; i < j; i++) {
+                    date = new Date(parseInt(data[i]["Timestamp"])*1000);
+                    hours.push(date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + ((date.getMinutes() < 10) ? "0"+date.getMinutes() : date.getMinutes()));
+
+                    players.push(parseInt(data[i]["PlayersOnline"]));
+                };
+
+                $('#graph').highcharts({
+                    title: {
+                        text: 'Playerbase',
+                        x: -20
+                    },
+                    subtitle: {
+                        text: 'Player history',
+                        x: -20
+                    },
+                    xAxis: { categories: hours },
+                    yAxis: {
+                        title: { text: 'Players' },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: [{
+                        name: 'Players',
+                        data: players
+                    }]
+                });
+            });
+        </script>
+    </body>
+</html>
